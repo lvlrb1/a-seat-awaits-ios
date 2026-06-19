a273efcdca8bc36b428cee62c35c07c920adf703
Brice Foster <brice.foster2.0@gmail.com>
2026-06-19 13:47:35 -0500

    Initial Commit

---
+++ A Seat Awaits/A_Seat_AwaitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Seat_AwaitsApp.swift
+//  A Seat Awaits
+//
+//  Created by Brice Foster on 6/19/26.
+//
+
+import SwiftUI
+
+@main
+struct A_Seat_AwaitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
